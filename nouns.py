f3e15b154bdc731c82a08f3d96c32c8eac8f789d
Forest Kobayashi <fkobayashi@hmc.edu>
2020-02-29 08:35:35 -0800

    partway through classifying nouns

---
+++ nouns.py
@@ -0,0 +1,149 @@
+# Here we have a classifcation of nouns based on whether they're
+# 1) inanimate objects,
+# 2) animate things (e.g., people),
+# 3) places
+# 4) times
+# 5) abstract things
+# 6) body parts
+
+inanimate = [
+    "chair",
+    "movie",
+    "toy",
+    "souvenir",
+    "camera",
+]
+
+animate = [
+    "younger sister",
+    "dog",
+    "cow",
+    "doctor",
+    "good child",
+    "(my) older sister",
+    "you",
+    "God",
+    "adult",
+    "older brother",
+    "older sister",
+    "grandmother; old woman",
+    "younger brother",
+    "family",
+    "man",
+    "boy",
+    "woman",
+    "girl",
+    "girlfriend",
+    "grandfather; old man",
+    "guardian deity of children",
+]
+
+become_able = [
+    "rich person",
+    "singer",
+    "office worker",
+    "astronaut",
+]
+
+do_able = [
+    "good deed",
+    "shopping",
+    "questionnaire",
+]
+
+make_able = [
+    "questionnaire",
+]
+
+drink_able = [
+    "green tea",
+    "sake; alcohol",
+]
+
+eat_able = [
+    "snack; sweets",
+    "sweet bun",
+    "ice cream",
+    "breakfast",
+    "rice cake",
+    "boxed lunch",
+]
+
+buy_able = drink_able + eat_able + []
+
+body_parts = [
+    "head",
+    "hair",
+    "shoulder",
+    "leg; foot",
+    "face",
+    "stomach",
+    "buttocks",
+]
+
+weather = [
+    "rain",
+]
+
+places_relative = [
+    "back",
+    "between",
+    "on",
+]
+
+# Sub-places within a place
+sub_places = [
+    "restroom",
+    "entrance",
+    "gate",
+    "corner",
+]
+
+places_absolute = [
+    "home; house",
+    "festival",
+    "home; house; my place",
+    "station",
+    "spa; hot spring",
+    "school",
+    "temple",
+    "castle",
+    "foreign country",
+    "Australia",
+    "Italy",
+    "apartment",
+]
+
+times_relative = [
+    "morning",
+    "the day before yesterday",
+    "breakfast",
+    "the year before last",
+    "tomorrow",
+    "the day after tomorrow",
+]
+
+times_absolute = [
+    "the fifth day of a month",
+    "New Year’s",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+    "June",
+    "fall",
+]
